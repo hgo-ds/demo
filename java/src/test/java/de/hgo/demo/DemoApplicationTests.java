6995c960503007e5cb38d0e1a44266859179094d
Heinrich Götzger EXT <Heinrich.Goetzger@partner.doubleslash.de>
2024-01-17 11:12:59 +0100

    initial commit of Spring boot demo; created with https://spring.io/quickstart/

---
+++ java/src/test/java/de/hgo/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package de.hgo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+    // empty on purpose
+  }
+
+}
